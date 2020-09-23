41c18d480762162eb583589c7403df4ebf8f7a67
Cameron Stuart <cameron@cameronstuart.com>
2020-09-23 11:51:04 -0400

    adds new VC

---
+++ DeckOfOneCard/View Controller/TestViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TestViewController.swift
+//  DeckOfOneCard
+//
+//  Created by Cameron Stuart on 9/23/20.
+//  Copyright © 2020 Warren. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+
+    @IBOutlet weak var testLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
